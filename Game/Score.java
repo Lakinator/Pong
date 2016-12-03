1d104b4723b86127dda731cdebe2bfb364514e6a
Lukas S <SchalkLukas20@gmail.com>
2016-12-03 21:02:18 +0100

    Update Score.java

--- Game/Score.java
+++ Game/Score.java
@@ -6,5 +6,30 @@ package Game;
  */
 
 public class Score {
-    private int playerLeftWinCount, playerRightWinCount;
+    private static int neededWins = 3;
+    private static int playerLeftWinCount = 0, playerRightWinCount = 0;
+
+    public static int getPlayerLeftWinCount() {
+        return playerLeftWinCount;
+    }
+
+    static void setPlayerLeftWinCount(int playerLeftWinCount) {
+        Score.playerLeftWinCount = playerLeftWinCount;
+    }
+
+    public static int getPlayerRightWinCount() {
+        return playerRightWinCount;
+    }
+
+    static void setPlayerRightWinCount(int playerRightWinCount) {
+        Score.playerRightWinCount = playerRightWinCount;
+    }
+
+    public static int getNeededWins() {
+        return neededWins;
+    }
+
+    static void setNeededWins(int neededWins) {
+        Score.neededWins = neededWins;
+    }
 }
