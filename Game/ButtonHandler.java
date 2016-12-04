15c087c4b07f7bd20563b3d356d62daef229940a
Lukas S <SchalkLukas20@gmail.com>
2016-12-04 15:48:39 +0100

    Update ButtonHandler.java

--- Game/ButtonHandler.java
+++ Game/ButtonHandler.java
@@ -11,43 +11,44 @@ import java.awt.event.ActionListener;
 class ButtonHandler implements ActionListener {
     @Override
     public void actionPerformed(ActionEvent e) {
-        if (e.getSource() == GUI.startBtn) {
+        if (e.getSource() == GUI.buttons[0]) {
             GameVar.menuScreen = false;
             GameVar.settingScreen = false;
             GameVar.endScreen = false;
             GameVar.gameScreen = true;
-        } else if (e.getSource() == GUI.settingBtn) {
+        } else if (e.getSource() == GUI.buttons[1]) {
             GameVar.menuScreen = false;
             GameVar.gameScreen = false;
             GameVar.endScreen = false;
             GameVar.settingScreen = true;
-            GUI.neededWinsField.setText(Score.getNeededWins() + " = Win Punktzahl (Enter zum speichern)");
+            GUI.textFields[0].setText(Score.getNeededWins() + "");
+            GUI.labels[GUI.toggleButtons.length].setText("Win Punktzahl (Enter zum speichern)(" + Score.getNeededWins() + "):");
         }
 
         //Settings
-        if (e.getSource() == GUI.paddleLeftBtnUp) {
-            if (GUI.paddleLeftBtnUp.isSelected()) {
-                GUI.paddleLeftBtnUp.setText("Drücke eine Taste");
-            } else if (!GUI.paddleLeftBtnUp.isSelected()) {
-                GUI.paddleLeftBtnUp.setText("PaddleLeftUpKey: " + GameVar.paddleLeft.getKeyCodeUp());
+        if (e.getSource() == GUI.toggleButtons[0]) {
+            if (GUI.toggleButtons[0].isSelected()) {
+                GUI.toggleButtons[0].setText("Drücke eine Taste");
+            } else if (!GUI.toggleButtons[0].isSelected()) {
+                GUI.toggleButtons[0].setText(GameVar.paddleLeft.getKeyCharUp() + " (" + GameVar.paddleLeft.getKeyCodeUp() + ")");
             }
-        } else if (e.getSource() == GUI.paddleLeftBtnDown) {
-            if (GUI.paddleLeftBtnDown.isSelected()) {
-                GUI.paddleLeftBtnDown.setText("Drücke eine Taste");
-            } else if (!GUI.paddleLeftBtnDown.isSelected()) {
-                GUI.paddleLeftBtnDown.setText("PaddleLeftDownKey: " + GameVar.paddleLeft.getKeyCodeDown());
+        } else if (e.getSource() == GUI.toggleButtons[1]) {
+            if (GUI.toggleButtons[1].isSelected()) {
+                GUI.toggleButtons[1].setText("Drücke eine Taste");
+            } else if (!GUI.toggleButtons[1].isSelected()) {
+                GUI.toggleButtons[1].setText(GameVar.paddleLeft.getKeyCharDown() + " (" + GameVar.paddleLeft.getKeyCodeDown() + ")");
             }
-        } else if (e.getSource() == GUI.paddleRightBtnUp) {
-            if (GUI.paddleRightBtnUp.isSelected()) {
-                GUI.paddleRightBtnUp.setText("Drücke eine Taste");
-            } else if (!GUI.paddleRightBtnUp.isSelected()) {
-                GUI.paddleRightBtnUp.setText("PaddleRightUpKey: " + GameVar.paddleRight.getKeyCodeUp());
+        } else if (e.getSource() == GUI.toggleButtons[2]) {
+            if (GUI.toggleButtons[2].isSelected()) {
+                GUI.toggleButtons[2].setText("Drücke eine Taste");
+            } else if (!GUI.toggleButtons[2].isSelected()) {
+                GUI.toggleButtons[2].setText(GameVar.paddleRight.getKeyCharUp() + " (" + GameVar.paddleRight.getKeyCodeUp() + ")");
             }
-        } else if (e.getSource() == GUI.paddleRightBtnDown) {
-            if (GUI.paddleRightBtnDown.isSelected()) {
-                GUI.paddleRightBtnDown.setText("Drücke eine Taste");
-            } else if (!GUI.paddleRightBtnDown.isSelected()) {
-                GUI.paddleRightBtnDown.setText("PaddleRightDownKey: " + GameVar.paddleRight.getKeyCodeDown());
+        } else if (e.getSource() == GUI.toggleButtons[3]) {
+            if (GUI.toggleButtons[3].isSelected()) {
+                GUI.toggleButtons[3].setText("Drücke eine Taste");
+            } else if (!GUI.toggleButtons[3].isSelected()) {
+                GUI.toggleButtons[3].setText(GameVar.paddleRight.getKeyCharDown() + " (" + GameVar.paddleRight.getKeyCodeDown() + ")");
             }
         }
     }
